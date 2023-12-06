0488d9a821c8d295ccc0521fe599dfb47be7a083
Ayush Maurya <amaurya5556@gmail.com>
2023-12-07 02:34:41 +0530

    Fix nav color and esummit fully responsive

--- summit23/script/wallfooter.js
+++ summit23/script/wallfooter.js
@@ -53,3 +53,17 @@ const navSlide = () => {
 }
 navSlide();
 // nav script ends here
+
+// to change the color of navbar from black to transparent on esummit website
+// after scrolling certain amount
+document.addEventListener("DOMContentLoaded", function() {
+  const nav = document.querySelector('nav');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) { // Adjust the scroll height as needed (100vh in this case)
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
+});
